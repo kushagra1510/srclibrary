808a71ad5d5c1014a61521bc78e46910b031d5f5
kushagra1510 <78354545+kushagra1510@users.noreply.github.com>
2021-05-21 17:41:11 +0530

    Update register.php

--- member/register.php
+++ member/register.php
@@ -9,7 +9,7 @@
 		<title>Register</title>
 		<link rel="stylesheet" type="text/css" href="../css/global_styles.css">
 		<link rel="stylesheet" type="text/css" href="../css/form_styles.css">
-		<!--link rel="stylesheet" href="css/register_style.css"-->
+		<link rel="stylesheet" href="css/register_style.css">
 	</head>
 	<body>
 		<form class="cd-form" method="POST" action="#">
