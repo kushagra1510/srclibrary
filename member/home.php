03b59d0f4978c9c9a821868ac8ca690e1060b2df
kushagra1510 <78354545+kushagra1510@users.noreply.github.com>
2021-05-21 17:42:09 +0530

    Update home.php

--- member/home.php
+++ member/home.php
@@ -9,8 +9,8 @@
 	<head>
 		<title>Welcome</title>
 		<link rel="stylesheet" type="text/css" href="../css/global_styles.css">
-		<!--link rel="stylesheet" type="text/css" href="css/home_style.css"-->
-		<!--link rel="stylesheet" type="text/css" href="../css/custom_radio_button_style.css"-->
+		<link rel="stylesheet" type="text/css" href="css/home_style.css">
+		<link rel="stylesheet" type="text/css" href="../css/custom_radio_button_style.css">
 	</head>
 	<body>
 		<?php
